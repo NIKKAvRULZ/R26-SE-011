f07b6efb8857e48ef1923b9390b42853416aecb3
Dilki687 <chamodidilki44@gmail.com>
2026-08-26 23:24:44 +0530

    merge component 4

--- component4-security-layer/frontend/src/App.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import VerificationPortal from './components/VerificationPortal-clean';
-
-export default function App() {
-  return <VerificationPortal />;
-}
--- component4-security-layer/frontend/src/components/index.js
+++
@@ -1,1 +0,0 @@
-export { default as VerificationPortal } from './VerificationPortal';
